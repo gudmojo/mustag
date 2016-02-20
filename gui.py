b33806386427eeac114965c65ded1efa0fbec8a4
Gudmundur Johannsson <gudmundur.orn@gmail.com>
2016-02-20 12:23:11 +0000

    Code formatting

--- gui.py
+++ gui.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import wx, wx.html
-import sys
 import wx.media
 import wx.lib.buttons as buttons
 import os
@@ -12,191 +11,172 @@ bitmapDir = os.path.join(dirName, 'bitmaps')
 
 class MainPanel(wx.Panel):
 
-	def __init__(self, parent):
-		wx.Panel.__init__(self, parent=parent)
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent)
  
-		self.frame = parent
-		self.currentVolume = 50
-		self.createMenu()
-		self.layoutControls()
-		self.library = mustag.Mustag()
-		self.library.load_meta_from_disk()
-		self.loadMusic('c:\\mp3_experiment\\WALK THE MOON - Shut Up and Dance.mp3')
+        self.frame = parent
+        self.currentVolume = 50
+        self.create_menu()
+        self.layout_controls()
+        self.library = mustag.Mustag()
+        self.library.load_meta_from_disk()
+        self.load_music('c:\\mp3_experiment\\WALK THE MOON - Shut Up and Dance.mp3')
   
-		self.timer = wx.Timer(self)
-		self.Bind(wx.EVT_TIMER, self.onTimer)
-		self.timer.Start(100)
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.on_timer)
+        self.timer.Start(100)
  
-	def layoutControls(self):
-		try:
-			self.mediaPlayer = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER)
-		except NotImplementedError:
-			self.Destroy()
-			raise
+    def layout_controls(self):
+        try:
+            self.mediaPlayer = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER)
+        except NotImplementedError:
+            self.Destroy()
+            raise
  
-		# create playback slider
-		self.playbackSlider = wx.Slider(self, size=wx.DefaultSize)
-		self.Bind(wx.EVT_SLIDER, self.onSeek, self.playbackSlider)
+        # create playback slider
+        self.playbackSlider = wx.Slider(self, size=wx.DefaultSize)
+        self.Bind(wx.EVT_SLIDER, self.on_seek, self.playbackSlider)
  
-		self.volumeCtrl = wx.Slider(self, style=wx.SL_VERTICAL|wx.SL_INVERSE)
-		self.volumeCtrl.SetRange(0, 100)
-		self.volumeCtrl.SetValue(self.currentVolume)
-		self.volumeCtrl.Bind(wx.EVT_SLIDER, self.onSetVolume)
+        self.volumeCtrl = wx.Slider(self, style=wx.SL_VERTICAL|wx.SL_INVERSE)
+        self.volumeCtrl.SetRange(0, 100)
+        self.volumeCtrl.SetValue(self.currentVolume)
+        self.volumeCtrl.Bind(wx.EVT_SLIDER, self.on_set_volume)
  
-		# Create sizers
-		mainSizer = wx.BoxSizer(wx.VERTICAL)
-		hSizer = wx.BoxSizer(wx.HORIZONTAL)
-		audioSizer = self.buildAudioBar()
+        # Create sizers
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        h_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        audioSizer = self.build_audio_bar()
  
-		# layout widgets
-		mainSizer.Add(self.playbackSlider, 1, wx.ALL|wx.EXPAND, 5)
-		hSizer.Add(audioSizer, 0, wx.ALL|wx.CENTER, 5)
-		hSizer.Add(self.volumeCtrl, 0, wx.ALL, 5)
-		mainSizer.Add(hSizer)
+        # layout widgets
+        main_sizer.Add(self.playbackSlider, 1, wx.ALL|wx.EXPAND, 5)
+        h_sizer.Add(audioSizer, 0, wx.ALL|wx.CENTER, 5)
+        h_sizer.Add(self.volumeCtrl, 0, wx.ALL, 5)
+        main_sizer.Add(h_sizer)
  
-		self.SetSizer(mainSizer)
-		self.Layout()
- 
+        self.SetSizer(main_sizer)
+        self.Layout()
 
-	def buildAudioBar(self):
-		audioBarSizer = wx.BoxSizer(wx.HORIZONTAL)
+    def build_audio_bar(self):
+        audio_bar_sizer = wx.BoxSizer(wx.HORIZONTAL)
  
-		self.buildBtn({'bitmap':'player_prev.png', 'handler':self.onPrev,
-					   'name':'prev'},
-					  audioBarSizer)
+        self.build_btn({'bitmap': 'player_prev.png', 'handler': self.on_skip_prev_song, 'name': 'prev'}, audio_bar_sizer)
  
-		# create play/pause toggle button
-		img = wx.Bitmap(os.path.join(bitmapDir, "player_play.png"))
-		self.playPauseBtn = buttons.GenBitmapToggleButton(self, bitmap=img, name="play")
-		self.playPauseBtn.Enable(False)
+        # create play/pause toggle button
+        img = wx.Bitmap(os.path.join(bitmapDir, "player_play.png"))
+        self.playPauseBtn = buttons.GenBitmapToggleButton(self, bitmap=img, name="play")
+        self.playPauseBtn.Enable(False)
  
-		img = wx.Bitmap(os.path.join(bitmapDir, "player_pause.png"))
-		self.playPauseBtn.SetBitmapSelected(img)
-		self.playPauseBtn.SetInitialSize()
+        img = wx.Bitmap(os.path.join(bitmapDir, "player_pause.png"))
+        self.playPauseBtn.SetBitmapSelected(img)
+        self.playPauseBtn.SetInitialSize()
  
-		self.playPauseBtn.Bind(wx.EVT_BUTTON, self.onPlay)
-		audioBarSizer.Add(self.playPauseBtn, 0, wx.LEFT, 3)
+        self.playPauseBtn.Bind(wx.EVT_BUTTON, self.on_play_song)
+        audio_bar_sizer.Add(self.playPauseBtn, 0, wx.LEFT, 3)
  
-		btnData = [{'bitmap':'player_stop.png',
-					'handler':self.onStop, 'name':'stop'},
-					{'bitmap':'player_next.png',
-					 'handler':self.onNext, 'name':'next'}]
-		for btn in btnData:
-			self.buildBtn(btn, audioBarSizer)
+        btn_data = [{'bitmap':'player_stop.png',
+                    'handler':self.on_stop, 'name': 'stop'},
+                    {'bitmap':'player_next.png',
+                     'handler':self.on_skip_song_forward, 'name': 'next'}]
+        for btn in btn_data:
+            self.build_btn(btn, audio_bar_sizer)
  
-		return audioBarSizer
- 
+        return audio_bar_sizer
 
-	def buildBtn(self, btnDict, sizer):
-		bmp = btnDict['bitmap']
-		handler = btnDict['handler']
+    def build_btn(self, btnDict, sizer):
+        bmp = btnDict['bitmap']
+        handler = btnDict['handler']
  
-		img = wx.Bitmap(os.path.join(bitmapDir, bmp))
-		btn = buttons.GenBitmapButton(self, bitmap=img, name=btnDict['name'])
-		btn.SetInitialSize()
-		btn.Bind(wx.EVT_BUTTON, handler)
-		sizer.Add(btn, 0, wx.LEFT, 3)
+        img = wx.Bitmap(os.path.join(bitmapDir, bmp))
+        btn = buttons.GenBitmapButton(self, bitmap=img, name=btnDict['name'])
+        btn.SetInitialSize()
+        btn.Bind(wx.EVT_BUTTON, handler)
+        sizer.Add(btn, 0, wx.LEFT, 3)
+
+    def create_menu(self):
+        menu_bar = wx.MenuBar()
  
+        file_menu = wx.Menu()
+        import_music_menu_item = file_menu.Append(wx.NewId(), "&Import music", "Import music")
+        reload_settings_file_menu_item = file_menu.Append(wx.NewId(), "&Reload settings", "Reload settings")
+        menu_bar.Append(file_menu, '&File')
+        self.frame.SetMenuBar(menu_bar)
+        self.frame.Bind(wx.EVT_MENU, self.on_import_music, import_music_menu_item)
+        self.frame.Bind(wx.EVT_MENU, self.on_reload_settings_file, reload_settings_file_menu_item)
 
-	def createMenu(self):
-		menubar = wx.MenuBar()
+    def load_music(self, music_file):
+        if not self.mediaPlayer.Load(music_file):
+            wx.MessageBox("Unable to load %s: Unsupported format?" % music_file, "ERROR", wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+            self.playPauseBtn.Enable(True)
+
+    def load_library(self, musicFile):
+        if not self.mediaPlayer.Load(musicFile):
+            wx.MessageBox("Unable to load %s: Unsupported format?" % musicFile,
+                          "ERROR",
+                          wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+            self.playPauseBtn.Enable(True)
+
+    def on_import_music(self, event):
+        pass
+
+    def on_reload_settings_file(self, event):
+        pass
  
-		fileMenu = wx.Menu()
-		import_music_menu_item = fileMenu.Append(wx.NewId(), "&Import music", "Import music")
-		reload_settings_file_menu_item = fileMenu.Append(wx.NewId(), "&Reload settings", "Reload settings")
-		menubar.Append(fileMenu, '&File')
-		self.frame.SetMenuBar(menubar)
-		self.frame.Bind(wx.EVT_MENU, self.onImportMusic, import_music_menu_item)
-		self.frame.Bind(wx.EVT_MENU, self.onReloadSettingsFile, reload_settings_file_menu_item)
+    def on_skip_song_forward(self, event):
+        pass
 
-		
-	def loadMusic(self, musicFile):
-		if not self.mediaPlayer.Load(musicFile):
-			wx.MessageBox("Unable to load %s: Unsupported format?" % musicFile,
-						  "ERROR",
-						  wx.ICON_ERROR | wx.OK)
-		else:
-			self.mediaPlayer.SetInitialSize()
-			self.GetSizer().Layout()
-			self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
-			self.playPauseBtn.Enable(True)
+    def on_pause_song(self):
+        self.mediaPlayer.Pause()
 
-			
-	def loadLibrary(self, musicFile):
-		if not self.mediaPlayer.Load(musicFile):
-			wx.MessageBox("Unable to load %s: Unsupported format?" % musicFile,
-						  "ERROR",
-						  wx.ICON_ERROR | wx.OK)
-		else:
-			self.mediaPlayer.SetInitialSize()
-			self.GetSizer().Layout()
-			self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
-			self.playPauseBtn.Enable(True)
-
-
-	def onImportMusic(self, event):
-		pass
+    def on_play_song(self, event):
+        if not event.GetIsDown():
+            self.on_pause_song()
+            return
  
-
-	def onReloadSettingsFile(self, event):
-		pass
+        if not self.mediaPlayer.Play():
+            wx.MessageBox("Unable to Play media : Unsupported format?",
+                          "ERROR",
+                          wx.ICON_ERROR | wx.OK)
+        else:
+            self.mediaPlayer.SetInitialSize()
+            self.GetSizer().Layout()
+            self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
  
-
-	def onNext(self, event):
-		pass
+        event.Skip()
  
-
-	def onPause(self):
-		self.mediaPlayer.Pause()
-
-
-	def onPlay(self, event):
-		if not event.GetIsDown():
-			self.onPause()
-			return
+    def on_skip_prev_song(self, event):
+        pass
  
-		if not self.mediaPlayer.Play():
-			wx.MessageBox("Unable to Play media : Unsupported format?",
-						  "ERROR",
-						  wx.ICON_ERROR | wx.OK)
-		else:
-			self.mediaPlayer.SetInitialSize()
-			self.GetSizer().Layout()
-			self.playbackSlider.SetRange(0, self.mediaPlayer.Length())
+    def on_seek(self, event):
+        offset = self.playbackSlider.GetValue()
+        self.mediaPlayer.Seek(offset)
  
-		event.Skip()
+    def on_set_volume(self, event):
+        self.currentVolume = self.volumeCtrl.GetValue()
+        print "setting volume to: %s" % int(self.currentVolume)
+        self.mediaPlayer.SetVolume(self.currentVolume)
  
-
-	def onPrev(self, event):
-		pass
+    def on_stop(self, event):
+        self.mediaPlayer.Stop()
+        self.playPauseBtn.SetToggle(False)
  
-
-	def onSeek(self, event):
-		offset = self.playbackSlider.GetValue()
-		self.mediaPlayer.Seek(offset)
- 
-
-	def onSetVolume(self, event):
-		self.currentVolume = self.volumeCtrl.GetValue()
-		print "setting volume to: %s" % int(self.currentVolume)
-		self.mediaPlayer.SetVolume(self.currentVolume)
- 
-
-	def onStop(self, event):
-		self.mediaPlayer.Stop()
-		self.playPauseBtn.SetToggle(False)
- 
-
-	def onTimer(self, event):
-		# Keep the player slider updated
-		offset = self.mediaPlayer.Tell()
-		self.playbackSlider.SetValue(offset)
+    def on_timer(self, event):
+        # Keep the player slider updated
+        offset = self.mediaPlayer.Tell()
+        self.playbackSlider.SetValue(offset)
  
 
 class MainFrame(wx.Frame):
- 	def __init__(self):
-		wx.Frame.__init__(self, None, wx.ID_ANY, "the mustag", pos=(10,10), size=(1500,800))
-		panel = MainPanel(self)
+    def __init__(self):
+        wx.Frame.__init__(self, None, wx.ID_ANY, "the mustag", pos=(10,10), size=(1500,800))
+        panel = MainPanel(self)
 
 
 app = wx.App(redirect=False)
--- mustag.py
+++ mustag.py
@@ -1,55 +1,54 @@
 import os
-import codecs
-import sys
 import json
+
 
 class Mustag:
 
-	def __init__(self):
-		self.basedir = 'c:\\mp3_experiment'
-		self.meta_extension = '.mustag'
-		self.library = dict()
+    def __init__(self):
+        self.basedir = 'c:\\mp3_experiment'
+        self.meta_extension = '.mustag'
+        self.library = dict()
 
-	# find all db files in basedir and load them into memory
-	def load_meta_from_disk(self):
-		for root, dirs, files in os.walk(self.basedir):
-			for file in files:
-				if file.endswith(self.meta_extension):
-					filepath = os.path.join(root, file)
-					self.library_add(filepath, file)
+    # find all db files in basedir and load them into memory
+    def load_meta_from_disk(self):
+        for root, dirs, files in os.walk(self.basedir):
+            for file in files:
+                if file.endswith(self.meta_extension):
+                    file_path = os.path.join(root, file)
+                    self.library_add(file_path, file)
 
-	# load legal tags from file, including color
+    # load legal tags from file, including color
 
-	# when triggered: import music from basedir
-	def import_music(self):
-		for root, dirs, files in os.walk(self.basedir):
-			for file in files:
-				if file.endswith(".mp3"):
-					filepath = os.path.join(root, file)
-					print(filepath.encode('unicode-escape'))
-					item = self.library_add(filepath, file)
-					self.create_metamusic(item)
+    # when triggered: import music from basedir
+    def import_music(self):
+        for root, dirs, files in os.walk(self.basedir):
+            for file_name in files:
+                if file_name.endswith(".mp3"):
+                    file_path = os.path.join(root, file_name)
+                    print(file_path.encode('unicode-escape'))
+                    item = self.library_add(file_path, file_name)
+                    self.create_music_metadata(item)
 
-	def library_add(self, filepath, file):
-		item = {
-			'filename': file,
-			'filepath': filepath,
-			'tags': []
-		}
-		self.library[filepath] = item
-		return item
+    def library_add(self, file_path, file_name):
+        item = {
+            'filename': file_name,
+            'filepath': file_path,
+            'tags': []
+        }
+        self.library[file_path] = item
+        return item
 
-	def create_metamusic(self, item):
-		metafilename = item['filepath'] + self.meta_extension
-		f = open(metafilename, 'w')
-		jsonstr = json.dumps(item)
-		f.write(jsonstr)
-		f.close()
+    def create_music_metadata(self, item):
+        metadata_filename = item['filepath'] + self.meta_extension
+        f = open(metadata_filename, 'w')
+        json_str = json.dumps(item)
+        f.write(json_str)
+        f.close()
 
-	# when triggered: reload legal tags from file
+    # when triggered: reload legal tags from file
 
-	# show a list of all songs and the tags on them
+    # show a list of all songs and the tags on them
 
-	# show a detail view of a song with buttons to toggle each tag
+    # show a detail view of a song with buttons to toggle each tag
 
-	# perform screen: toggle tags and songs will be randomly selected
+    # perform screen: toggle tags and songs will be randomly selected
